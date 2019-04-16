7ea0d094fea6d93417ca19c30482f5d642816cfb
Davi - FullSolution <Davi@full.com>
2019-04-16 19:53:44 -0300

    Agregada pagina de autor

---
+++ prueba.js
@@ -0,0 +1,16 @@
+$( document ).ready(function() {
+
+    async function getPost() {
+        try {
+             await fetch('https://jsonplaceholder.typicode.com/todos/1')
+            .then(response => response.json())
+            .then(json => console.log(json));
+        }
+        catch(e) {
+            console.log(e);
+        }
+        // Siempre trabajarlo con try catch
+        return response;
+    };
+    $('#posts').text(getPost())
+});
